49176429826bd5e7c70267fc163dfa5328119ff1
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2011-11-30 20:01:31 +0100

    Started celery Cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Rafael Durán Castañeda"
+maintainer_email "rafadurancastaneda@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures celery"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
